db28bafebec7f9053cb3895888784927eb6a368f
qq378287007 <qq378287007@163.com>
2024-02-27 14:24:28 +0800

    a

---
+++ 5/Service.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <unistd.h>
+#include "Sunnet.h"
+#include "Service.h"
+
+Service::Service()
+{
+    pthread_spin_init(&queueLock, PTHREAD_PROCESS_PRIVATE);
+    pthread_spin_init(&inGlobalLock, PTHREAD_PROCESS_PRIVATE);
+}
+
+Service::~Service()
+{
+    pthread_spin_destroy(&queueLock);
+    pthread_spin_destroy(&inGlobalLock);
+}
+
+// 插入消息
+void Service::PushMsg(shared_ptr<BaseMsg> msg)
+{
+    pthread_spin_lock(&queueLock);
+    msgQueue.push(msg);
+    pthread_spin_unlock(&queueLock);
+}
+
+// 取出消息
+shared_ptr<BaseMsg> Service::PopMsg()
+{
+    shared_ptr<BaseMsg> msg = nullptr;
+    pthread_spin_lock(&queueLock);
+    if (!msgQueue.empty())
+    {
+        msg = msgQueue.front();
+        msgQueue.pop();
+    }
+    pthread_spin_unlock(&queueLock);
+    return msg;
+}
+
+// 处理一条消息，返回值代表是否处理
+bool Service::ProcessMsg()
+{
+    shared_ptr<BaseMsg> msg = PopMsg();
+    if (msg)
+    {
+        OnMsg(msg);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// 处理N条消息，返回值代表是否处理
+void Service::ProcessMsgs(int max)
+{
+    for (int i = 0; i < max; i++)
+    {
+        bool succ = ProcessMsg();
+        if (!succ)
+            break;
+    }
+}
+
+// 创建服务后触发
+void Service::OnInit()
+{
+    cout << "[" << id << "] OnInit" << endl;
+}
+
+// 收到消息时触发
+void Service::OnMsg(shared_ptr<BaseMsg> msg)
+{
+    // 测试用
+    if (msg->type == BaseMsg::TYPE::SERVICE)
+    {
+        shared_ptr<ServiceMsg> m = dynamic_pointer_cast<ServiceMsg>(msg);
+        cout << "[" << id << "] OnMsg " << m->buff << endl;
+
+        sleep(1);
+        shared_ptr<BaseMsg> msgRet = Sunnet::MakeMsg(id, new char[9999999]{'p', 'i', 'n', 'g', '\0'}, 9999999);
+        Sunnet::inst()->Send(m->source, msgRet);
+    }
+    else
+    {
+        cout << "[" << id << "] OnMsg" << endl;
+    }
+}
+
+// 退出服务时触发
+void Service::OnExit()
+{
+    cout << "[" << id << "] OnExit" << endl;
+}
+
+void Service::SetInGlobal(bool isIn)
+{
+    pthread_spin_lock(&inGlobalLock);
+    inGlobal = isIn;
+    pthread_spin_unlock(&inGlobalLock);
+}
---
+++ 5/Service.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <queue>
+#include <thread>
+using namespace std;
+#include "Msg.h"
+
+class Service
+{
+public:
+    uint32_t id;             // 唯一id
+    shared_ptr<string> type; // 类型
+    bool isExiting = false;  // 是否正在退出
+
+    // 消息列表
+    queue<shared_ptr<BaseMsg>> msgQueue;
+    pthread_spinlock_t queueLock;
+
+    // 标记是否在全局队列  true:在队列中，或正在处理
+    bool inGlobal = false;
+    pthread_spinlock_t inGlobalLock;
+
+public:
+    Service();
+    ~Service();
+
+    // 回调函数（编写服务逻辑）
+    void OnInit();
+    void OnMsg(shared_ptr<BaseMsg> msg);
+    void OnExit();
+
+    // 插入消息
+    void PushMsg(shared_ptr<BaseMsg> msg);
+
+    // 执行消息
+    bool ProcessMsg();
+    void ProcessMsgs(int max);
+
+    // 全局队列
+    void SetInGlobal(bool isIn);
+
+private:
+    // 取出一条消息
+    shared_ptr<BaseMsg> PopMsg();
+};
